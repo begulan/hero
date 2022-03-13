e0edee2b7ab690df38b8f19b0712d1472bdffc49
Begulan <begulan@bk.ru>
2022-03-13 17:09:57 +0600

    topmenu

--- client/components/TopMenu.vue
+++ client/components/TopMenu.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="top-menu">
+    <div class="d-flex justify-content-center align-items-center">
       <nuxt-link to="/">Starters</nuxt-link>
+      <span class="dot"><i class="bi bi-circle-fill"></i></span>
       <nuxt-link to="/lunch">Lunch</nuxt-link>
+      <span class="dot"><i class="bi bi-circle-fill"></i></span>
       <nuxt-link to="/deserts">Deserts</nuxt-link>
-      <nuxt-link to="/youorder">You Order (0)</nuxt-link>
+      <span class="dot"><i class="bi bi-circle-fill"></i></span>
+      <nuxt-link to="/youorder">You Order <i class="bi bi-bag"></i> (0)</nuxt-link>
+    </div>
   </div>
 </template>
 
@@ -14,5 +19,26 @@ export default {
 </script>
 
 <style lang="scss">
-
+.top-menu {
+  margin: 20px auto;
+  a {
+    color: #333;
+    border-bottom: 1px solid transparent;
+    text-transform: uppercase;
+    font-size: 20px;
+    text-decoration: none;
+    &.nuxt-link-exact-active {
+      border-bottom: 1px solid #333;
+    }&:hover {
+      border-bottom: 1px solid #333;
+    }
+  }
+  .dot {
+    color: #333;
+    font-size: 8px;
+    line-height: 0;
+    display: inline-block;
+    padding: 0 15px
+  }
+}
 </style>
